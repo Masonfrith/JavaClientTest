0fd0b8ddbabb36664907f4b0a3a635e05f48016d
Masonfrith <gedsaro@gmail.com>
2018-10-17 18:04:20 -0600

    add client start up stuff in main, this is likely temp...
    add attempt to send message to server upon connection attempt.
    add some error messages.
    add closing of the things at the end.

--- JavaClientTest/src/Main/Main.java
+++ JavaClientTest/src/Main/Main.java
@@ -1,10 +1,13 @@
 package Main;
 
+import client.MyClient;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting up the client");
+		MyClient client = new MyClient();
+		System.out.println("Does CLIENT Reach this last line of code?");
 	}
 
 }
--- JavaClientTest/src/client/MyClient.java
+++ JavaClientTest/src/client/MyClient.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -13,28 +14,40 @@ public class MyClient {
 	BufferedReader in;
 	
 
-	public MyClient(String ip, int portNumber) {
+	public MyClient(String ip, int portNumber) throws Exception {
 		this.connectToServer(ip, portNumber);
 	}
 	
-	public MyClient() {
+	public MyClient() throws Exception {
 		
-		this.connectToServer("defaultIPgoesHere", 8002); // a default attempt at connection
+		this.connectToServer("localhost", 40008); // a default attempt at connection
 	}
 	
-	public void connectToServer(String ip, int portNumber) {
+	public void connectToServer(String ip, int portNumber) throws Exception {
+		System.out.println("Client now attempting to connect to server at ip: " + ip + " and port " + portNumber);
 		try {
 			clientSocket = new Socket(ip, portNumber);
 		out = new PrintWriter(clientSocket.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		
+		out.println("Hi I am client, I try to talk to you!");
+		} catch (ConnectException e) {
+			e.printStackTrace();
+			System.out.println("Error, could not connect to server using ip: " + ip + " and portNumber " + portNumber);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("Error, Unknown host exception trying to connect to server using ip: " + ip + " and portNumber " + portNumber);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
+		
+		System.out.println("Client, does it reach this line in connectToServer?");
+		
+		in.close();
+		out.close();
+		clientSocket.close();
+		
 	}
 
 }
